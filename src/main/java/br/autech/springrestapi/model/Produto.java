558938f0e5c1f2c292f4847e109178e70618840a
Vinícius Maia <viniciusmaiasg@gmail.com>
2025-10-13 16:38:13 -0300

    alterando cliente e criando produto
    # Conflicts:
    #	src/main/java/br/autech/springrestapi/model/Cliente.java

---
+++ src/main/java/br/autech/springrestapi/model/Produto.java
@@ -0,0 +1,38 @@
+package br.autech.springrestapi.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "PRODUTO")
+public class Produto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID_PRODUTO")
+    private String idProduto;
+    private String nome;
+    private BigDecimal valor;
+    @ManyToMany(fetch = FetchType.LAZY)
+    private Set<Cliente> clientes = new HashSet<>();
+
+    public Produto() {
+    }
+
+    public Produto(String nome, BigDecimal valor) {
+        super();
+        this.nome = nome;
+        this.valor = valor;
+
+    }
+
+
+}
